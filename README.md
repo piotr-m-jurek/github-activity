0846d738958bb6e46a71be2fcc46e0304cbe2e5c
Piotr Malecki-Jurek <piotr.m.jurek@checkhq.com>
2025-01-24 21:14:37 +0100

    update readme and formatters

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # github-activity
+
+Project made for learning path from [Roadmap][https://roadmap.sh]
+
+Apologies for one commit. Took me not so long time, didn't commit in the meantime.
+
+## Caveats
+
+One point that I changed from roadmap's description in my implementation is:
+
+> Do not use any external libraries or frameworks to build this project.
+
+OCaml tends to be the language where, if you don't write your own standard library, you are not really programming.
+Therefore I settled on using
+- [yojson](https://github.com/ocaml-community/yojson) for parsing JSON file
+- [core](https://opensource.janestreet.com/core/) standard library from Jane Street
+- [lwt](https://github.com/ocsigen/lwt) for promises and concurrency
+
+## Installation
+
+Make sure you have OCaml installed on your machine [Instructions](https://ocaml.org/install)
+
+Install deps:
+```sh
+opam install --deps-only --yes .
+```
+
+Build project:
+
+```sh
+opam exec -- dune build
+```
+
+Run:
+```sh
+opam exec -- dune exec github_user_activity piotr-m-jurek
+```
+
--- lib/AggregatedData.ml
+++ lib/AggregatedData.ml
@@ -1,54 +1,47 @@
 open Core
 
-type t =
-  { count : int
-  ; repo : string
-  }
-
 let aggregate_by_type ~(event : UserActivity.t) ~events =
-  let rec aux (events : (Activity.t * t) list) =
+  let rec aux (events : (Activity.t * int) list) =
     match events with
-    | [] -> [ event.activity_type, { count = 0; repo = event.repo.name } ]
-    | (key, data) :: tail
+    | [] -> [ event.activity_type, 0 ]
+    | (key, count) :: tail
       when String.equal (Activity.to_string key) (Activity.to_string event.activity_type)
-      -> (key, { data with count = data.count + 1 }) :: tail
+      -> (key, count + 1) :: tail
     | pair :: tail -> pair :: aux tail
   in
   aux events
 ;;
 
-let event_string ~data:{ count; repo } ~activity =
+let event_string ~count ~activity =
   let open Activity in
   match activity with
-  | CommitCommentEvent ->
-    sprintf "User added Commit comment to repo %s %d times" repo count
-  | CreateEvent -> sprintf "User created repo %s %d times" repo count
-  | DeleteEvent -> sprintf "User deleted repo %s %d times" repo count
-  | ForkEvent -> sprintf "Forked repo %s %d times" repo count
-  | GollumEvent -> sprintf "User added GollumEvent to repo %s %d times" repo count
-  | IssueCommentEvent -> sprintf "Issue commented on repo %s %d times" repo count
-  | IssuesEvent -> sprintf "IssuesEvent on repo %s %d times" repo count
-  | MemberEvent -> sprintf "MemberEvent on repo %s %d times" repo count
-  | PublicEvent -> sprintf "PublicEvent on repo %s %d times" repo count
-  | PullRequestEvent -> sprintf "PullRequestEvent on repo %s %d times" repo count
-  | PullRequestReviewCommentEvent ->
-    sprintf "PullRequestReviewCommentEvent on repo %s %d times" repo count
-  | PullRequestReviewEvent -> sprintf "PullRequestReviewEvent"
-  | PushEvent -> sprintf "PushEvent on repo %s %d times" repo count
-  | ReleaseEvent -> sprintf "ReleaseEvent on repo %s %d times" repo count
-  | SponsorshipEvent -> sprintf "SponsorshipEvent on repo %s %d times" repo count
-  | WatchEvent -> sprintf "WatchEvent on repo %s %d times" repo count
+  | CommitCommentEvent -> sprintf "Added commit comment to  %d times" count
+  | CreateEvent -> sprintf "User created repo  %d times" count
+  | DeleteEvent -> sprintf "User deleted repo %d times" count
+  | ForkEvent -> sprintf "Forked repo %d times" count
+  | GollumEvent -> sprintf "Created/Updated wiki to %d times" count
+  | IssueCommentEvent -> sprintf "Issue commented on %d times" count
+  | IssuesEvent -> sprintf "Interacted with issues %d times" count
+  | MemberEvent -> sprintf "Collaborated %d times" count
+  | PublicEvent -> sprintf "Published repository %d times" count
+  | PullRequestEvent -> sprintf "Created pull request  %d times" count
+  | PullRequestReviewCommentEvent -> sprintf "Commented on pull request %d times" count
+  | PullRequestReviewEvent -> sprintf "Reviewed pull request %d times" count
+  | PushEvent -> sprintf "Pushed commit on %d times" count
+  | ReleaseEvent -> sprintf "Release repo on %d times" count
+  | SponsorshipEvent -> sprintf "Sponsorship on %d times" count
+  | WatchEvent -> sprintf "Watched on %d times" count
 ;;
 
 let join_with_newline s1 s2 = s1 ^ "\n" ^ s2
 
-let make (events : UserActivity.events) : (Activity.t * t) list =
+let make events : (Activity.t * int) list =
   List.fold events ~init:[] ~f:(fun acc event -> aggregate_by_type ~event ~events:acc)
 ;;
 
 let print_summary data =
-  Printf.printf "\n%s" "User activity summary:\n\n";
+  Printf.printf "\n%s" "User activity summary:";
   make data
-  |> List.fold ~init:"" ~f:(fun acc (activity, data) ->
-    event_string ~data ~activity |> join_with_newline acc)
+  |> List.fold ~init:"" ~f:(fun acc (activity, count) ->
+    event_string ~count ~activity |> join_with_newline acc)
 ;;
